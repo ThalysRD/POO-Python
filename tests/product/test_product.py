0eea3a3fc9992378961b0698cd6af28a1ac2e834
Thalys Rodrigues <thalysrd@pop-os.localdomain>
2023-01-09 17:47:16 -0300

    Feat: Cria testes para a classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    start = '21-12-2010'
+    end = '21-12-2023'
+    product = Product(325, 'Tinta', 'Trybe', start, end, '1', 'bla')
+
+    print(type(product.id))
+    assert isinstance(product.id, int)
+    assert isinstance(product.nome_do_produto, str)
+    assert isinstance(product.nome_da_empresa, str)
+    assert isinstance(product.data_de_fabricacao, str)
+    assert isinstance(product.data_de_validade, str)
+    assert isinstance(product.numero_de_serie, str)
+    assert isinstance(product.instrucoes_de_armazenamento, str)
