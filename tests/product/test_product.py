9b28ea85652d883eefb8622743f77e497be8fc75
Thalys Rodrigues <thalysrd@pop-os.localdomain>
2023-01-11 16:20:41 -0300

    Fix: Correção do test product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -4,13 +4,12 @@ from inventory_report.inventory.product import Product
 def test_cria_produto():
     start = '21-12-2010'
     end = '21-12-2023'
-    product = Product(325, 'Tinta', 'Trybe', start, end, '1', 'bla')
+    product = Product(325, 'Tinta', 'Armazém', start, end, '1', 'bla')
 
-    print(type(product.id))
-    assert isinstance(product.id, int)
-    assert isinstance(product.nome_do_produto, str)
-    assert isinstance(product.nome_da_empresa, str)
-    assert isinstance(product.data_de_fabricacao, str)
-    assert isinstance(product.data_de_validade, str)
-    assert isinstance(product.numero_de_serie, str)
-    assert isinstance(product.instrucoes_de_armazenamento, str)
+    assert product.id == 325
+    assert product.nome_do_produto == 'Tinta'
+    assert product.nome_da_empresa == 'Armazém'
+    assert product.data_de_fabricacao == start
+    assert product.data_de_validade == end
+    assert product.numero_de_serie == '1'
+    assert product.instrucoes_de_armazenamento == 'bla'
